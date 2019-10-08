a2e8bb61c2de815a38de69a239988e1f8010b253
ali1ariel <aamachado@inf.ufsm.br>
2019-10-08 12:22:32 -0300

    add system of validation data.

--- src/pages/EditPerson.jsx
+++ src/pages/EditPerson.jsx
@@ -34,82 +34,115 @@ export default function EditPerson({ match, history }) {
   const [bairro, setBairro] = useState('');
   const [cidade, setCidade] = useState('');
   const [uf, setUf] = useState('');
+  const [mensagem, setMensagem] = useState('');
+  const [cepValido, setCepValido] = useState(false);
 
-  function cepValidado(enderecoSet) {
-    setLogradouro(enderecoSet.logradouro);
-    setBairro(enderecoSet.bairro);
-    setCidade(enderecoSet.localidade);
-    setUf(enderecoSet.uf);
+  async function cepValidado(endereco) {
+    if (endereco.erro) {
+      setLogradouro("");
+      setBairro("");
+      setCidade("");
+      setUf("");
+      setMensagem("O CEP é inválido, por favor, verifique!");
+    }
+    setLogradouro(endereco.logradouro);
+    setBairro(endereco.bairro);
+    setCidade(endereco.localidade);
+    setUf(endereco.uf);
+    setCepValido(true);
   }
-
 
   return (
     <React.Fragment>
       <div>
-      <h1 className="title-component">Editar pessoa</h1>
+        <h1 className="title-component">Editar pessoa</h1>
+        <h2 className="info-text">{mensagem}</h2>
         <form className="person-form">
-        <b className="grid text">nome: </b><input
+          <b className="grid text">nome: </b><input
             type="text"
             className="grid grid-3"
             value={nome || person.nome}
-            onChange={e => { setNome(e.target.value) }}
-            required
-          /> 
+            onChange={e => {
+              setNome(e.target.value);
+              setMensagem("");
+            }}
+          />
           <b className="grid text">CPF: </b><input
             type="text"
             className="grid grid-3"
             value={cpf || person.cpf}
-            onChange={e => { setCpf(e.target.value) }}
-          /> 
+            onChange={e => {
+              setCpf(e.target.value);
+              console.log(cpf);
+              setMensagem("");
+            }}
+          />
           <b className="grid text">e-mail: </b><input
             type="text"
             className="grid grid-3"
             value={email || person.email}
-            onChange={e => { setEmail(e.target.value) }}
-          /> 
+            onChange={e => {
+              setEmail(e.target.value)
+                ; setMensagem("");
+            }}
+          />
           <b className="grid text">CEP: </b><input
             type="text"
             className="grid grid-2"
             value={cep || person.cep}
-            onChange={e => { setCep(e.target.value) }}
+            onChange={e => {
+              setCep(e.target.value);
+              setMensagem("");
+              setCepValido(false);
+            }}
+
           /> <input
             type="button" className="grid button"
             value="validar CEP"
-            onClick={async e => {
-              const result = await (validaCEP(cep));
-              cepValidado(result.data);
+            onClick={async () => {
+              if (cep && cep.length === 8) {
+                const result = await (validaCEP(cep));
+                console.log(result);
+                cepValidado(result.data);
+              } else {
+                setMensagem("O campo de CEP está com número errado de caracteres, impossível validar!")
+              }
             }}
           />
           <b className="grid text">Logradouro: </b><input
             type="text"
-            className="grid grid-3"
+            className="grid grid-3 not-required"
             value={logradouro || endereco.logradouro} readOnly
-          /> 
+          />
 
           <b className="grid text">número: </b><input
             type="text"
             className="grid"
             value={numero || person.numero}
-            onChange={e => { setNumero(e.target.value) }}
-          /> 
+
+            onChange={e => {
+              setNumero(e.target.value);
+              setMensagem("");
+            }}
+          />
 
           <b className="grid text">bairro: </b><input
             type="text"
-            className="grid"
+            className="grid not-required"
             value={bairro || endereco.bairro} readOnly
-          /> 
+          />
 
           <b className="grid text">cidade: </b><input
             type="text"
-            className="grid"
+            className="grid not-required"
             value={cidade || endereco.localidade} readOnly
-          /> 
+          />
           <b className="grid text">Estado: </b><input
             type="text"
-            className="grid"
+            className="grid not-required"
             value={uf || endereco.uf} readOnly
           /> <br />
-          <img src={back} alt="Voltar à lista" onClick={ () => history.push("/persons")}/>  
+          <img src={back} alt="Voltar à lista" onClick={() => history.push("/persons")} />
           <br />
           <img className="enviar" src={send} alt="Enviar" onClick={() => {
 
@@ -119,20 +152,42 @@ export default function EditPerson({ match, history }) {
                 "cpf": cpf || person.cpf,
                 "email": email || person.email,
                 "cep": cep || person.cep,
-                "numero": parseInt(numero) || person.numero
+                "numero": parseInt(numero) || parseInt(person.numero)
               }
             }
 
             async function putPessoa(data) {
-              console.log(data)
-              await api.put(`/${person.id}`, data);
-            }
+              try {
+                if (cepValido !== false) {
 
+                  if (cpf.length !== 11) {
+                    setMensagem(`CPF inválido, o número de caracteres é 11(Contém ${cpf.length})`)
+                  } else if (!email.includes("@")) {
+                    setMensagem("Não parece um e-mail, não contem '@'")
+                  } else {
+                    const response = await api.put(`/${person.id}`, data);
+                    console.log(response);
+                    history.push('/');
+                  }
+                } else {
+                  setMensagem("Valide o cep e verifique se está tudo preenchido!")
+                }
+
+              } catch (error) {
+                // Error 😨
+                console.log(error);
+                if (error.response.status === 422) {
+                  const dados = [nome, cpf, email, cep, numero];
+                  if (dados.filter((el) => { return el })) {
+                    setMensagem("Há dados que não estão preenchidos.")
+                  };
+                };
+              };
+            };
             putPessoa(pessoa);
-
-            history.push('/persons');
-
-          }} />
+          }
+        }
+        />
         </form>
 
       </div>
--- src/pages/NewPerson.jsx
+++ src/pages/NewPerson.jsx
@@ -18,14 +18,25 @@ const NewPerson = ({ history }) => {
   const [bairro, setBairro] = useState('');
   const [cidade, setCidade] = useState('');
   const [uf, setUf] = useState('');
+  const [mensagem, setMensagem] = useState('');
+  const [cepValido, setCepValido] = useState(false);
 
 
 
   async function cepValidado(endereco) {
+    if (endereco.erro) {
+
+      setLogradouro("");
+      setBairro("");
+      setCidade("");
+      setUf("");
+      setMensagem("O CEP é inválido, por favor, verifique!");
+    }
     setLogradouro(endereco.logradouro);
     setBairro(endereco.bairro);
     setCidade(endereco.localidade);
     setUf(endereco.uf);
+    setCepValido(true);
   }
 
 
@@ -34,77 +45,99 @@ const NewPerson = ({ history }) => {
 
       <div>
         <h1 className="title-component">Adicionar pessoa</h1>
+        <h2 className="info-text">{mensagem}</h2>
         <form className="person-form">
           <b className="grid text">nome: </b><input
             type="text"
             className="grid grid-3"
             value={nome}
-            onChange={e => { setNome(e.target.value) }}
-            required
+            onChange={e => {
+              setNome(e.target.value);
+              setMensagem(""); 
+            }}
+            
           />
           <b className="grid text">CPF: </b><input
             type="text"
             className="grid grid-3"
             value={cpf}
-            onChange={e => { setCpf(e.target.value) }}
-            required
-          /> 
+            onChange={e => {
+              setCpf(e.target.value); 
+              setMensagem("");
+            }}
+            
+          />
           <b className="grid text">e-mail: </b><input
             type="text"
             className="grid grid-3"
             value={email}
-            onChange={e => { setEmail(e.target.value) }}
-            required
-          /> 
+            onChange={e => {
+              setEmail(e.target.value)
+              ; setMensagem("");
+            }}
+            
+          />
           <b className="grid text">CEP: </b><input
             type="text"
             className="grid grid-2"
             value={cep}
-            onChange={e => { setCep(e.target.value) }}
-            required
+            onChange={e => {
+              setCep(e.target.value); 
+              setMensagem(""); 
+              setCepValido(false);
+            }}
+            
           /> <input className="grid button"
             type="button"
             value="validar CEP"
             onClick={async () => {
-              const result = await (validaCEP(cep));
-              cepValidado(result.data);
+              if(cep && cep.length===8){
+                const result = await (validaCEP(cep));
+                console.log(result);
+                cepValidado(result.data);
+              } else {
+                setMensagem("O campo de CEP está com número errado de caracteres, impossível validar!")
+              }
             }}
-            required
+            
           />
           <b className="grid text">Logradouro: </b><input
             type="text"
-            className="grid grid-3"
+            className="grid grid-3 not-required"
             value={logradouro} readOnly
-            required
-          /> 
+            
+          />
 
           <b className="grid text">número: </b><input
             type="text"
             className="grid"
             value={numero}
-            onChange={e => { setNumero(e.target.value) }}
-          /> 
+            onChange={e => {
+              setNumero(e.target.value);
+              setMensagem("");
+              }}
+          />
 
           <b className="grid text">bairro: </b><input
             type="text"
-            className="grid"
+            className="grid not-required"
             value={bairro} readOnly
-          /> 
+          />
 
           <b className="grid text">cidade: </b><input
             type="text"
-            className="grid"
+            className="grid not-required"
             value={cidade} readOnly
           />
           <b className="grid text">Estado: </b><input
             type="text"
-            className="grid"
+            className="grid not-required"
             value={uf} readOnly
           />  <br />
-          <img src={back} alt="Voltar à lista" onClick={ () => history.push("/persons")}/>  
+          <img src={back} alt="Voltar à lista" onClick={() => history.push("/persons")} />
           <br />
           <img className="enviar" src={send} alt="Enviar" onClick={() => {
-            
+
             const pessoa = {
               "person": {
                 "nome": nome,
@@ -116,12 +149,36 @@ const NewPerson = ({ history }) => {
             }
 
             async function postPessoa(data) {
-              await api.post('', data);
+              try {
+                if(cepValido!==false){
+
+                  if(cpf.length!== 11){
+                    setMensagem(`CPF inválido, o número de caracteres é 11(Contém ${cpf.length})`)
+                  } else if (!email.includes("@")) {
+                    setMensagem("Não parece um e-mail, não contem '@'")
+                  } else {
+                    const response = await api.post('', data);
+                    console.log(response);
+                    history.push('/');
+                  }
+                } else {
+                  setMensagem("Valide o cep e verifique se está tudo preenchido!")
+                }
+
+              } catch (error) {
+                // Error 😨
+                console.log(error);
+                if (error.response.status === 422) {
+                  const dados = [ nome, cpf, email, cep, numero ];
+                  if(dados.filter((el) => {return el})) {
+                    setMensagem("Há dados que não estão preenchidos.")
+                  };
+                } 
+              }
             }
 
             postPessoa(pessoa);
 
-            history.push('/');
 
 
 
